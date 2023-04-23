fa221e8c32c36cb3166663f78a2594fc53503bc0
stanlocktop <wlad-test@ukr.net>
2023-04-23 21:30:48 +0300

    add carousel

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+        autoplay: true,
+        autoplayTimeout: 1450,
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
