b44b39208a39279c14ee4883058927ceaafe3fcb
stanlocktop <wlad-test@ukr.net>
2023-04-29 20:02:18 +0300

    add mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,72 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
         autoplay: true,
         autoplayTimeout: 1450,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 2
+            },
+            767: {
+                items: 3
             },
 
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
     });
 });
+
+
+
+
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+
+    //Navigation
+
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("light")) {
+            $("body").removeClass("light");
+            $("#switch").removeClass("switched");
+        }
+        else {
+            $("body").addClass("light");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
+
+
